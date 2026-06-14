2a909cc4d1b99cc5663406ef1c019aac6337a690
Bedram Tamang <tmgbedu@gmail.com>
2026-06-14 10:22:30 -0700

    feat: add testing and upgrade to the latest versions

---
+++ tests/test_case.py
@@ -0,0 +1,7 @@
+from fastapi_startkit.fastapi.testing import HttpTestCase
+
+
+class TestCase(HttpTestCase):
+    def get_application(self):
+        from bootstrap.application import app
+        return app
